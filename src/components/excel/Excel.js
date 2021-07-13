a1a4babc64de39e3370215a071df0e373784feff
Alim Sadetov <sadetovalim@gmail.com>
2021-07-13 13:59:57 +0300

    export components

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,20 @@ export class Excel {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
   }
+  //функция которая создаёт див и в нём контент который берётся с каждой компоненты
+  //дом переменные обозначаются с знака доллара
+  getRoot() {
+    const $root = document.createElement('div')
+
+    this.components.forEach(Component => {
+      const component = new Component()
+      $root.insertAdjacentHTML('beforeend', component.toHTML())
+    })
+
+    return $root
+  }
+  //функция которая в апп добавляет полученное в гетрут
+  render() {
+    this.$el.append(this.getRoot())
+  }
 }
